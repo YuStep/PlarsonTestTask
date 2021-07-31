72d22ca8733a31997b0cc96e602aa1277776ce92
YuStep <63791090+YuStep@users.noreply.github.com>
2021-07-31 23:19:35 +0300

    Delete testproject directory

--- testproject/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('.burger-button').click(function(){
-        $('.user-list').toggleClass('open');
-        $('.burger-button').toggleClass('open');
-    });
-    
-  });
