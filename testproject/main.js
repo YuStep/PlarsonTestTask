187ab8344a822c44fd6aee7bbc8e8f7b30c76efd
YuStep <63791090+YuStep@users.noreply.github.com>
2021-07-31 23:14:14 +0300

    add files

---
+++ testproject/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.burger-button').click(function(){
+        $('.user-list').toggleClass('open');
+        $('.burger-button').toggleClass('open');
+    });
+    
+  });
